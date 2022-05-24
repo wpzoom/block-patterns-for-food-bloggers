b30ca59e266ad504310c9fd5f84cab0f343f874d
Alexei Samarschi <Samarschi@gmail.com>
2022-05-24 16:26:48 +0300

    Fixed JS in content editor

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -1,56 +1,27 @@
-(function ($) {
-    'use strict';
+// wrapped into IIFE - to leave global space clean.
+( function( window, wp ){
 
-    var $window = $(window);
+	//console.log( wp.hooks );
+    // just to keep it cleaner - we refer to our link by id for speed of lookup on DOM.
 
-    /**
-    * Document ready (jQuery)
-    */
-    $(function () {
+    // check if gutenberg's editor root element is present.
+    var editorEl = document.getElementById( 'editor' );
 
-        $('.wpz_pattern_16 .wp-block-query li .wp-block-group__inner-container').each(function(){
-           $(this).children().not(':last').wrapAll("<div class='wpz-posts-inner-info'></div>");
+    if( !editorEl ){ // do nothing if there's no gutenberg root element on page.
+        return;
+    }
 
-        });
+	var unsubscribe = wp.data.subscribe( function () {
 
-        $('.wpz_pattern_17 .wp-block-query li .wp-block-group__inner-container').each(function(){
-           $(this).children().not(':last').wrapAll("<div class='wpz-posts-inner-info'></div>");
+        setTimeout( function () {
 
-        });
-
+			jQuery('.wpz_pattern_16 .wp-block-query li .wp-block-group__inner-container, .wpz_pattern_17 .wp-block-query li .wp-block-group__inner-container').each(function(){
+				if( !jQuery(this).children().hasClass('wpz-posts-inner-info') ) {
+					jQuery(this).children().not(':last').wrapAll("<div class='wpz-posts-inner-info'></div>");
+				}
+	 
+			 });
+	 
+		}, 1 )
     });
-
-    $window.ready(function() {
-
-        $.fn.queryLoopLayout();
-
-    });
-
-    $(document).ready(function() {
-        $.fn.queryLoopLayout();
-    });
-
-    $(window).resize(function(){
-        $.fn.queryLoopLayout();
-    });
-
-
-    $.fn.queryLoopLayout = function () {
-        $(window).on('resize orientationchange', update);
-
-        function update() {
-            $('.wpz_pattern_16 .wp-block-query li .wp-block-group__inner-container').each(function(){
-               $(this).children().not(':last').wrapAll("<div class='wpz-posts-inner-info'></div>");
-
-            });
-
-            $('.wpz_pattern_17 .wp-block-query li .wp-block-group__inner-container').each(function(){
-               $(this).children().not(':last').wrapAll("<div class='wpz-posts-inner-info'></div>");
-
-            });
-        }
-
-        update();
-    };
-
-})(jQuery);
+})( window, wp );
--- assets/js/utils.js
+++ assets/js/utils.js
@@ -8,17 +8,10 @@
     * Document ready (jQuery)
     */
     $(function () {
-
-        $('.wpz_pattern_16 .wp-block-query li .wp-block-group__inner-container').each(function(){
+        $('.wpz_pattern_16 .wp-block-query li .wp-block-group__inner-container, .wpz_pattern_17 .wp-block-query li .wp-block-group__inner-container').each(function(){
            $(this).children().not(':last').wrapAll("<div class='wpz-posts-inner-info'></div>");
 
         });
-
-        $('.wpz_pattern_17 .wp-block-query li .wp-block-group__inner-container').each(function(){
-           $(this).children().not(':last').wrapAll("<div class='wpz-posts-inner-info'></div>");
-
-        });
-
     });
 
 
